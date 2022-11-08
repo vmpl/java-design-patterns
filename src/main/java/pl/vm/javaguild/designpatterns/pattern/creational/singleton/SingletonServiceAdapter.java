ea2a7f5d264f241aa2ca9ba1e2e7ca71710eafff
tkluza <tomasz.kluza@vm.pl>
2022-11-08 16:40:05 +0200

    fix: Small structure refactor

---
+++ src/main/java/pl/vm/javaguild/designpatterns/pattern/creational/singleton/SingletonServiceAdapter.java
@@ -0,0 +1,8 @@
+package pl.vm.javaguild.designpatterns.pattern.creational.singleton;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SingletonServiceAdapter implements SingletonService {
+
+}
